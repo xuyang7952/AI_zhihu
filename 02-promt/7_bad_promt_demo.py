49f02a91755841bdba66be7d352405b5493ca447
yang.xu <15505524365@163.com>
2024-04-10 18:57:08 +0800

    03--funccall

--- 02-promt/7_bad_promt_demo.py
+++ 02-promt/7_bad_promt_demo.py
@@ -108,3 +108,23 @@ response = client.moderations.create(
 )
 moderation_output = response.results[0].categories
 print_json(moderation_output)
+
+# 阿里的tongyiqinawei测试
+#  快速排序 
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+# 冒泡排序
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
